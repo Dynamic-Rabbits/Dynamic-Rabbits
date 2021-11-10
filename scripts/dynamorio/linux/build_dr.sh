f60b2cec59a97b2165cb886cbb184a9440de3c55
johnfxgalea <john.galea@stx.ox.ac.uk>
2021-11-10 18:14:15 +0000

    Arrange dr build scripts

--- scripts/dynamorio/linux/build_dr.sh
+++ scripts/dynamorio/linux/build_dr.sh
@@ -1,9 +1,7 @@
 git clone https://github.com/johnfxgalea/dynamorio.git $DYNAMORIO_DIR
 
 cd $DYNAMORIO_DIR
-git checkout my_mods
 git checkout 6a5a9f60a63ee922f053217202c815f76d0149de
-
 
 rm -rf build
 rm -rf install
@@ -30,10 +28,4 @@ else
   make install
   echo "NOT in Debug Mode"
 fi
-
-cd $DYNAMORIO_DIR/api/samples
-mkdir build
-cd build
-cmake -DX86_32=true -DCMAKE_BUILD_TYPE=Release -DDynamoRIO_DIR=$DYNAMORIO_DIR/install/cmake/ -DDYNAMORIO_SOURCE_DIR=$DYNAMORIO_DIR -DCMAKE_INSTALL_PREFIX=`pwd`/../install ..
-make -j 4
 
